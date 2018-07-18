903b3dad478e7c90f9afb26f00e2a6c5aa196f87
liujie <liu_jie_1@126.com>
2018-07-18 16:39:06 +0800

    add models to admin

--- apps/BaseModels/admin.py
+++ apps/BaseModels/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomerManage , TractorManage, Supplier ,GasManage ,TrailerManage,StaffManage,UserManage
 
 # Register your models here.
+admin.site.register(CustomerManage )
+admin.site.register(TractorManage )
+admin.site.register(Supplier )
+admin.site.register(GasManage )
+admin.site.register(TrailerManage )
+admin.site.register(StaffManage )
+admin.site.register(UserManage )
--- apps/BussinessModels/admin.py
+++ apps/BussinessModels/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import SalesList , MaterialPurchase ,VehicleMaintenanceManage ,WastageManage ,CustomPaymentInfo
 # Register your models here.
+admin.site.register(SalesList )
+admin.site.register(MaterialPurchase )
+admin.site.register(VehicleMaintenanceManage )
+admin.site.register(WastageManage )
+admin.site.register(CustomPaymentInfo )
