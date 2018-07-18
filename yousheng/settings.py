cb08aa532d8353262409978114b802a6088d60cd
liujie <liu_jie_1@126.com>
2018-07-18 16:24:50 +0800

    add django-suit , beautiful admin backend . 2018年07月18日

--- yousheng/settings.py
+++ yousheng/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.BaseModels',
     'apps.BussinessModels',
+
 ]
 
 MIDDLEWARE = [
