eadec29f17d8bc63cb6524d05bb792d2e41078d1
dvstrom <dvstrom@gmail.com>
2018-08-26 16:30:40 +0800

    add 采购表业务逻辑

--- apps/BussinessModels/models.py
+++ apps/BussinessModels/models.py
@@ -3,50 +3,17 @@
 
 
 from django.db import models
-
+from apps.BaseModels.models import CustomerManage,TractorManage,TrailerManage,StaffManage,UserManage,GasManage,Supplier
+import apps.BaseModels.models
 
 # Create your models here.
 
 
-# salesList【销售单】
-# 销售单编号		salesListID
-# 客户名称		customName
-# 客户编号		customID
-# 客户余额【不可手工编辑】【非表字段， 只是页面展示的显示】		customBalance
-# 采购单编号	默认为空	purchaseID
-# 品种		category
-# 拖车号		tractorID
-# 挂车号		trailerID
-# 司机		driverName
-# 押运员		supercargo
-# 数量  	【默认显示挂车余量】	count
-# 单位 【非表字段， 只是页面展示的显示】	吨/立方米
-# 单价		unitPrice
-# 码表公里数		mileage
-# 日期		date
-# 备注		comment
-# 是否已开发票（是/否）		isInvoiced
-# 本单是否入库（是/否）	未入库销售单可修改 ， 已入库销售单只能浏览不能修改。【分支业务：超级管理员可更改已入库的销售单信息 ， 不能删除】	isStoraged
 
 
-class SalesList(models.Model):
-    salesListID = models.CharField(max_length=100)
-    customName = models.CharField(max_length=100)
-    customID = models.CharField(max_length=100)
-    purchaseID = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    tractorID = models.CharField(max_length=100)
-    trailerID = models.CharField(max_length=100)
-    driverName = models.CharField(max_length=100)
-    supercargo = models.CharField(max_length=100)
-    count = models.CharField(max_length=100)
-    unitPrice = models.CharField(max_length=100)
-    mileage = models.CharField(max_length=100)
-    date = models.DateField()
-    comment = models.CharField(max_length=100)
-    isInvoiced = models.CharField(max_length=100)
-    isStoraged = models.CharField(max_length=100)
-
+class TestModle(models.Model):
+    testFiled = models.CharField(max_length=100, default="1")
+    testFiled2 = models.CharField(max_length=100, default="1")
 
 # materialPurchase 【采购单表】
 # 采购单编号	时间日期+流水ID	purchaseID
@@ -76,6 +43,56 @@ class MaterialPurchase(models.Model):
     mileage = models.CharField(max_length=100)
     date = models.DateField()
     isStoraged = models.CharField(max_length=100)
+
+# salesList【销售单】
+# 销售单编号		salesListID
+# 客户名称		customName
+# 客户编号		customID
+# 客户余额【不可手工编辑】【非表字段， 只是页面展示的显示】		customBalance
+# 采购单编号	默认为空	purchaseID
+# 品种		category
+# 拖车号		tractorID
+# 挂车号		trailerID
+# 司机		driverName
+# 押运员		supercargo
+# 数量  	【默认显示挂车余量】	count
+# 单位 【非表字段， 只是页面展示的显示】	吨/立方米
+# 单价		unitPrice
+# 码表公里数		mileage
+# 日期		date
+# 备注		comment
+# 是否已开发票（是/否）		isInvoiced
+# 本单是否入库（是/否）	未入库销售单可修改 ， 已入库销售单只能浏览不能修改。【分支业务：超级管理员可更改已入库的销售单信息 ， 不能删除】	isStoraged
+
+class SalesList(models.Model):
+    # salesListID = models.CharField(max_length=100)
+    #customName = models.CharField(max_length=100,default="")
+    # customID = models.CharField(max_length=100)
+    # purchaseID = models.CharField(max_length=100)
+    # category = models.CharField(max_length=100)
+    # tractorID = models.CharField(max_length=100)
+    # trailerID = models.CharField(max_length=100)
+    # driverName = models.CharField(max_length=100)
+    # supercargo = models.CharField(max_length=100)
+    # count = models.CharField(max_length=100)
+    # unitPrice = models.CharField(max_length=100)
+    # mileage = models.CharField(max_length=100)
+    # date = models.DateField()
+    # comment = models.CharField(max_length=100)
+    #isInvoiced = models.CharField(max_length=100)
+    # isStoraged = models.CharField(max_length=100)
+    salesListID = models.CharField(max_length=100)
+    custom = models.ForeignKey(CustomerManage, on_delete=models.CASCADE,default='')
+    purchaseID = models.ForeignKey(MaterialPurchase, default='',on_delete=models.CASCADE)
+    #category = models.ForeignKey(GasManage,on_delete=models.CASCADE,default='')
+    tractor = models.ForeignKey(TractorManage, on_delete=models.CASCADE,default='')
+    trailer = models.ForeignKey(TrailerManage, on_delete=models.CASCADE,default='')
+    @property
+    def customID(self):
+        return self.custom.customID
+
+
+
 
 # vehicleMaintenanceManage【车辆维修安排（统计）】
 # 类型（拖车/挂车）		vehicleType
